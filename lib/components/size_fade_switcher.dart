611b2386e613882078d86b051944e53003ade6a7
Victor Adepoju <victoradepoju30@gmail.com>
2022-08-16 06:25:11 +0100

    fadeTransition in sizeTransition with staggered animation

--- lib/components/size_fade_switcher.dart
+++ lib/components/size_fade_switcher.dart
@@ -11,6 +11,35 @@ class SizeFadeSwitcher extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedSwitcher(
+      transitionBuilder: (child, animation) {
+        final sizeAnimation = Tween<double>(begin: 0, end: 1).animate(
+          CurvedAnimation(
+            //CurvedAnimatino for Staggered Animation
+            parent: animation,
+            curve: const Interval(
+              0.0,
+              0.5,
+            ),
+          ),
+        );
+
+        final fadeAnimation = Tween<double>(begin: 0, end: 1).animate(
+          CurvedAnimation(
+            parent: animation,
+            curve: const Interval(
+              0.5,
+              0.1,
+            ),
+          ),
+        );
+        return FadeTransition(
+          opacity: fadeAnimation,
+          child: SizeTransition(
+            sizeFactor: sizeAnimation,
+            child: child,
+          ),
+        );
+      },
       duration: const Duration(milliseconds: 400),
       switchInCurve: Curves.easeIn,
       switchOutCurve: Curves.easeIn,
