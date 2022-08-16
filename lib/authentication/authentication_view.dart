ebf8853bdfc7b68f6b2e810b9406d2e547949502
Victor Adepoju <victoradepoju30@gmail.com>
2022-08-17 00:36:13 +0100

    signin, signup, forgot password, google siginin

--- lib/authentication/authentication_view.dart
+++ lib/authentication/authentication_view.dart
@@ -1,4 +1,5 @@
 import 'package:auth_firebase_riverpod/components/animated_button.dart';
+import 'package:auth_firebase_riverpod/components/auth_switch_button.dart';
 import 'package:auth_firebase_riverpod/components/text_input_field.dart';
 import 'package:flutter/material.dart';
 
@@ -16,50 +17,50 @@ class _AuthenticationViewState extends State<AuthenticationView> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 250, 16, 0),
-        child: Column(
-          children: [
-            TextInputField(
-              hintText: 'Email',
-              onChanged: (_) {},
-            ),
-            TextInputField(
-              hintText: 'Password',
-              onChanged: (_) {},
-            ),
-            AnimatedButton(
-              onTap: () {},
-              child: Container(
-                padding: const EdgeInsets.symmetric(vertical: 18),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Color(0xFFF5D973),
-                  borderRadius: BorderRadius.circular(16),
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 250, 16, 0),
+            child: Column(
+              children: [
+                TextInputField(
+                  hintText: 'Email',
+                  onChanged: (_) {},
                 ),
-                child: const Text(
-                  'Sign In',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
+                TextInputField(
+                  hintText: 'Password',
+                  onChanged: (_) {},
+                ),
+                AnimatedButton(
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 18),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF5D973),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: const Text(
+                      'Sign In',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
                   ),
                 ),
-              ),
+              ],
             ),
-            GestureDetector(
+          ),
+          AuthSwitchButton(
+              showSignIn: _showSignIn,
               onTap: () {
                 setState(() {
                   _showSignIn = !_showSignIn;
                 });
-              },
-              child: const Padding(
-                padding: EdgeInsets.all(16),
-                child: Text('Toggle'),
-              ),
-            ),
-          ],
-        ),
+              }),
+        ],
       ),
     );
   }
