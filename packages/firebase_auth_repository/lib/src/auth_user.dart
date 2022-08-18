38931102847b5850778a0d3333ce61b53c4a5000
Victor Adepoju <victoradepoju30@gmail.com>
2022-08-18 22:28:02 +0100

    created AuthUser model

---
+++ packages/firebase_auth_repository/lib/src/auth_user.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+class AuthUser extends Equatable {
+  final String id;
+  final String? email;
+  final String? name;
+  final bool emailVerified;
+  const AuthUser({
+    required this.id,
+    this.email,
+    this.name,
+    this.emailVerified = false,
+  });
+
+  static const empty = AuthUser(id: '');
+
+  bool get isEmpty => this == empty;
+
+  @override
+  List<Object?> get props => [
+        email,
+        id,
+        name,
+        emailVerified,
+      ];
+}
