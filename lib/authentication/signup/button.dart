8fa2f372ed7717703c0e0c23bdfafc7b60980865
Victor Adepoju <victoradepoju30@gmail.com>
2022-08-18 18:16:45 +0100

    used controller for signup button

--- lib/authentication/signup/button.dart
+++ lib/authentication/signup/button.dart
@@ -1,16 +1,24 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:form_validators/form_validators.dart';
 
 import '../../components/animated_button.dart';
 import '../../components/rounded_button_style.dart';
+import 'controller/signup_controller.dart';
 
-class SignUpButton extends StatelessWidget {
+class SignUpButton extends ConsumerWidget {
   const SignUpButton({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final signUpState = ref.watch(signUpProvider);
+    final signUpController = ref.read(signUpProvider.notifier);
+    final bool isValidated = signUpState.status.isValidated;
     return AnimatedButton(
-      onTap: () {},
+      onTap: isValidated
+          ? () => signUpController.signUpWithEmailAndPassword
+          : null,
       child: const RoundedButtonStyle(title: 'Sign Up'),
     );
   }
