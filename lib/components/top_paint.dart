412dad745f16ab0127f7708721825708fc7755d2
Victor Adepoju <victoradepoju30@gmail.com>
2022-08-18 03:23:58 +0100

    replaced paints y values with tweens for animation

--- lib/components/top_paint.dart
+++ lib/components/top_paint.dart
@@ -48,15 +48,40 @@ class BackgroundPainter extends CustomPainter {
     final paint = Paint()..color = backPaintColor; //rgb(88,190,230)
     final path = Path()
       ..moveTo(shapeBounds.topLeft.dx, shapeBounds.topLeft.dy)
-      ..lineTo(shapeBounds.left, shapeBounds.height * 0.42)
-      ..lineTo(shapeBounds.width * 0.58, shapeBounds.height * 0.75)
+      ..lineTo(
+        shapeBounds.left,
+        Tween<double>(begin: 0, end: shapeBounds.height * 0.42)
+            .animate(controller)
+            .value,
+      )
+      ..lineTo(
+        shapeBounds.width * 0.58,
+        Tween<double>(begin: 0, end: shapeBounds.height * 0.75)
+            .animate(controller)
+            .value,
+      )
       ..quadraticBezierTo(
-          shapeBounds.width - (shapeBounds.width / 4),
-          shapeBounds.bottom,
-          shapeBounds.bottomRight.dx,
-          shapeBounds.bottomRight.dy)
-      ..lineTo(shapeBounds.topRight.dx, shapeBounds.topRight.dy)
-      ..lineTo(shapeBounds.topLeft.dx, shapeBounds.topLeft.dy)
+        shapeBounds.width - (shapeBounds.width / 4),
+        Tween<double>(begin: 0, end: shapeBounds.bottom)
+            .animate(controller)
+            .value,
+        shapeBounds.bottomRight.dx,
+        Tween<double>(begin: 0, end: shapeBounds.bottomRight.dy)
+            .animate(controller)
+            .value,
+      )
+      ..lineTo(
+        shapeBounds.topRight.dx,
+        Tween<double>(begin: 0, end: shapeBounds.topRight.dy)
+            .animate(controller)
+            .value,
+      )
+      ..lineTo(
+        shapeBounds.topLeft.dx,
+        Tween<double>(begin: 0, end: shapeBounds.topLeft.dy)
+            .animate(controller)
+            .value,
+      )
       ..close();
 
     canvas.drawPath(path, paint);
@@ -65,27 +90,67 @@ class BackgroundPainter extends CustomPainter {
   void _paintBlackCurve(Canvas canvas, Rect shapeBounds) {
     final paint = Paint()..color = frontPaintColor;
     final path = Path()
-      ..moveTo(shapeBounds.left, shapeBounds.height * 0.42)
-      ..lineTo(shapeBounds.left, shapeBounds.height * 0.75)
-      ..quadraticBezierTo(shapeBounds.width * 0.5, shapeBounds.bottom,
-          shapeBounds.width * 0.58, shapeBounds.height * 0.75)
+      ..moveTo(
+        shapeBounds.left,
+        Tween<double>(begin: 0, end: shapeBounds.height * 0.42)
+            .animate(controller)
+            .value,
+      )
+      ..lineTo(
+        shapeBounds.left,
+        Tween<double>(begin: 0, end: shapeBounds.height * 0.75)
+            .animate(controller)
+            .value,
+      )
       ..quadraticBezierTo(
-          (shapeBounds.width - (shapeBounds.width / 6)),
-          shapeBounds.height * 0.28,
-          shapeBounds.right,
-          shapeBounds.height * 0.28)
-      ..lineTo(shapeBounds.topRight.dx, shapeBounds.topRight.dy)
-      ..lineTo(shapeBounds.width * 0.54, shapeBounds.top)
+        shapeBounds.width * 0.5,
+        Tween<double>(begin: 0, end: shapeBounds.bottom)
+            .animate(controller)
+            .value,
+        shapeBounds.width * 0.58,
+        Tween<double>(begin: 0, end: shapeBounds.height * 0.75)
+            .animate(controller)
+            .value,
+      )
       ..quadraticBezierTo(
-          shapeBounds.width * 0.28,
-          shapeBounds.height * 0.26 * 0.2,
-          shapeBounds.width * 0.25,
-          shapeBounds.height * 0.26)
+        (shapeBounds.width - (shapeBounds.width / 6)),
+        Tween<double>(begin: 0, end: shapeBounds.height * 0.28)
+            .animate(controller)
+            .value,
+        shapeBounds.right,
+        Tween<double>(begin: 0, end: shapeBounds.height * 0.28)
+            .animate(controller)
+            .value,
+      )
+      ..lineTo(
+        shapeBounds.topRight.dx,
+        Tween<double>(begin: 0, end: shapeBounds.topRight.dy)
+            .animate(controller)
+            .value,
+      )
+      ..lineTo(
+        shapeBounds.width * 0.54,
+        Tween<double>(begin: 0, end: shapeBounds.top).animate(controller).value,
+      )
+      ..quadraticBezierTo(
+        shapeBounds.width * 0.28,
+        Tween<double>(begin: 0, end: shapeBounds.height * 0.26 * 0.2)
+            .animate(controller)
+            .value,
+        shapeBounds.width * 0.25,
+        Tween<double>(begin: 0, end: shapeBounds.height * 0.26)
+            .animate(controller)
+            .value,
+      )
       ..quadraticBezierTo(
         shapeBounds.width / 5,
-        shapeBounds.height * 0.45,
+        Tween<double>(begin: 0, end: shapeBounds.height * 0.45)
+            .animate(controller)
+            .value,
         shapeBounds.left,
-        shapeBounds.height * 0.42,
+        Tween<double>(begin: 0, end: shapeBounds.height * 0.42)
+            .animate(controller)
+            .value,
       )
       ..close();
 
@@ -94,6 +159,7 @@ class BackgroundPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(BackgroundPainter oldDelegate) {
-    return frontPaintColor != oldDelegate.frontPaintColor;
+    return true;
+    // frontPaintColor != oldDelegate.frontPaintColor;
   }
 }
