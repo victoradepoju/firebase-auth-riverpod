84bf0c186de2b2a2b56720c3e185401c8b6208f4
Victor Adepoju <victoradepoju30@gmail.com>
2022-08-18 23:24:02 +0100

    completed authenticaton_repository package

--- packages/firebase_auth_repository/lib/src/authentication_repository.dart
+++ packages/firebase_auth_repository/lib/src/authentication_repository.dart
@@ -41,4 +41,72 @@ class AuthenticationRepository {
       }
     });
   }
+
+  Future<void> signUpWithEmailAndPassword(
+      {required String email, required String password}) async {
+    try {
+      await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      throw SignUpWithEmailAndPasswordFailure(e.code);
+    }
+  }
+
+  Future<void> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      throw SignInWithEmailAndPasswordFailure(e.code);
+    }
+  }
+
+  Future<void> forgotPassword({required String email}) async {
+    try {
+      await _firebaseAuth.sendPasswordResetEmail(email: email);
+    } on FirebaseAuthException catch (e) {
+      throw ForgotPasswordFailure(e.code);
+    }
+  }
+
+  Future<bool?> signInWithGoogle() async {
+    try {
+      final googleSignInAccount = await _googleSignIn.signIn();
+      if (googleSignInAccount == null) {
+        throw SignInWithGoogleFailure();
+      }
+
+      final googleSignInAuth = await googleSignInAccount.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuth.accessToken,
+        idToken: googleSignInAuth.idToken,
+      );
+
+      final userCredential =
+          await _firebaseAuth.signInWithCredential(credential);
+      // check if user is new, since user can be existing or new
+
+      return userCredential.additionalUserInfo?.isNewUser;
+    } on FirebaseAuthException catch (_) {
+      throw SignInWithGoogleFailure();
+    }
+  }
+
+  Future<void> signOut() async {
+    try {
+      await Future.wait({
+        _firebaseAuth.signOut(),
+        _googleSignIn.signOut(),
+      });
+    } catch (_) {
+      throw SignOutFailure();
+    }
+  }
 }
