0c6c7a649e02b8a00ced24ea20de971871e5267e
Victor Adepoju <victoradepoju30@gmail.com>
2022-08-19 05:10:15 +0100

    created authentication_state.dart file

---
+++ lib/authentication/controller/authentication_controller.dart
@@ -0,0 +1,5 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:equatable/equatable.dart';
+
+part 'authentication_state.dart';
