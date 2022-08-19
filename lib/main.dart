a59bc481f6a7893cae8963ca45a679e49a95f8c4
Victor Adepoju <victoradepoju30@gmail.com>
2022-08-19 05:41:32 +0100

    used landing page logic in main.dart for redirection

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:auth_firebase_riverpod/authentication/authentication_view.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'authentication/controller/authentication_controller.dart';
+import 'profile/profile.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,17 +15,30 @@ void main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authenticationState = ref.watch(authProvider);
+
+    Widget getHome() {
+      if (authenticationState.status == AuthenticationStatus.authenticated) {
+        return const Profile();
+      } else if (authenticationState.status ==
+          AuthenticationStatus.unauthenticated) {
+        return const AuthenticationView();
+      } else {
+        return const AuthenticationView();
+      }
+    }
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Auth Riverpod',
       theme: ThemeData(
         canvasColor: const Color(0xFFFFFEF4),
       ),
-      home: const AuthenticationView(),
+      home: getHome(),
     );
   }
 }
