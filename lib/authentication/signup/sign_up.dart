0a9757bdbd0b2bccabb91c611edb144c5beeee74
Victor Adepoju <victoradepoju30@gmail.com>
2022-08-19 20:48:25 +0100

    updated profile view and ref.listen to signup

--- lib/authentication/signup/sign_up.dart
+++ lib/authentication/signup/sign_up.dart
@@ -2,23 +2,36 @@ import 'package:auth_firebase_riverpod/authentication/signup/button.dart';
 import 'package:auth_firebase_riverpod/authentication/signup/email_field.dart';
 import 'package:auth_firebase_riverpod/authentication/signup/name_field.dart';
 import 'package:auth_firebase_riverpod/authentication/signup/password_field.dart';
-import 'package:auth_firebase_riverpod/components/animated_button.dart';
-import 'package:auth_firebase_riverpod/components/text_input_field.dart';
+import 'package:auth_firebase_riverpod/components/loading_error.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SignUp extends StatelessWidget {
+import 'package:form_validators/form_validators.dart';
+import 'controller/signup_controller.dart';
+
+class SignUp extends ConsumerWidget {
   const SignUp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<SignUpState>(signUpProvider, (previous, next) {
+      if (next.status.isSubmissionInProgress) {
+        LoadingSheet.show(context);
+      } else if (next.status.isSubmissionFailure) {
+        Navigator.of(context).pop();
+        ErrorDialog.show(context, "${next.errorMessage}");
+      } else if (next.status.isSubmissionSuccess) {
+        Navigator.of(context).pop();
+      }
+    });
     return Column(
-      children: [
+      children: const [
         NameField(),
-        const SizedBox(height: 16),
+        SizedBox(height: 16),
         EmailField(),
-        const SizedBox(height: 16),
+        SizedBox(height: 16),
         PasswordField(),
-        const SizedBox(height: 24),
+        SizedBox(height: 24),
         SignUpButton(),
       ],
     );
--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,21 +1,29 @@
+import 'package:auth_firebase_riverpod/authentication/controller/authentication_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends ConsumerWidget {
   const Profile({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authController = ref.watch(authProvider.notifier);
+    // authProvider return AuthController which is of type SignUpState.
+    // SignUpState has 'user' [AuthUser] property.
+    final authUser = ref.watch(authProvider).user;
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text('user id'),
-            Text('user email'),
-            Text('email verified'),
+            Text('user id: ${authUser.id}'),
+            Text('user email: ${authUser.email}'),
+            Text('email verified: ${authUser.emailVerified}'),
             TextButton(
-              onPressed: () {},
+              onPressed: () {
+                authController.onSignOut();
+              },
               child: const Text('Signout'),
             ),
           ],
